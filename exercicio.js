bde7933da1f06e0e50adcf46020110df9d9d10e5
Marlene Sedano <marlenesedanodasilva@gmail.com>
2020-12-12 19:30:57 -0400

    Tabuada do 9 ultilizando estrutura de repetição

--- exercicio.js
+++ exercicio.js
@@ -1,7 +1,21 @@
-let n1 = 5;
-let n2 = 7;
-let n3 = 10;
-let n4 = 4;
+let n1 = 10;
+let n2 = 10;
+let n3 = 8;
+let n4 = 8;
 let media = 0;
 mediaFinal = (n1 + n2 + n3 + n4) / 4;
-console.log(mediaFinal);
+//console.log(mediaFinal);
+
+
+//Introdução a portugol
+//Exercicio para tabuada do 9 ultilizando estrutura de repetição
+
+let numero = 0;
+let limite = 90;
+let i = 0;
+
+while (numero < limite) {
+    numero = numero + 9;
+    i++;
+    console.log("9 X " + i + " = " + numero);
+}
